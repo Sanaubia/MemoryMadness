f03b05e2ec83123feb7dc59877308ebec7b4ac27
Sanaubia <jprokkola@gmail.com>
2026-01-30 11:41:59 +0200

    Card selector created

--- Globals/SignalHub.gd
+++ Globals/SignalHub.gd
@@ -2,9 +2,13 @@ extends Node
 
 signal on_level_selected(level_num: int)
 signal on_game_exit_pressed
+signal on_tile_selected(tile: MemoryTile)
 
 func emit_on_level_selected(level_num: int) -> void:
 	on_level_selected.emit(level_num)
 
 func emit_on_game_exit_pressed() -> void:
 	on_game_exit_pressed.emit()
+
+func emit_on_tile_selected(tile: MemoryTile) -> void:
+	on_tile_selected.emit(tile)
--- Scenes/MemoryTile/MemoryTile.gd
+++ Scenes/MemoryTile/MemoryTile.gd
@@ -5,14 +5,19 @@ class_name MemoryTile
 @onready var frame_image: TextureRect = $FrameImage
 @onready var item_image: TextureRect = $ItemImage
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	reveal(false)
 
+func reveal(r: bool) -> void:
+	frame_image.visible = r
+	item_image.visible = r
+	
 func setup(image: Texture2D, frame: Texture2D) -> void:
 	frame_image.texture = frame
 	item_image.texture = image
 
 func _on_pressed() -> void:
-	pass # Replace with function body.
+	if Scorer.SelectionEnabled == true:
+		reveal(true)
+		SignalHub.emit_on_tile_selected(self)
