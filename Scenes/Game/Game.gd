0cff62a5ed81d48a10247ac479e4372b45c01942
Sanaubia <jprokkola@gmail.com>
2026-02-09 13:13:40 +0200

    Game UI label update

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -5,6 +5,8 @@ const MEMORY_TILE = preload("res://Scenes/MemoryTile/MemoryTile.tscn")
 @onready var tile_grid: GridContainer = $HBoxContainer/TileGrid
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var scorer: Scorer = $Scorer
+@onready var moves_label: Label = $HBoxContainer/MarginContainer/VBoxContainer/HBoxContainer/MovesLabel
+@onready var pairs_label: Label = $HBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/PairsLabel
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,12 +15,16 @@ func _ready() -> void:
 
 func _enter_tree() -> void:
 	SignalHub.on_level_selected.connect(on_level_selected)
-	
+
+func _process(delta: float) -> void:
+	moves_label.text = scorer.get_moves_made_str()
+	pairs_label.text = scorer.get_pairs_made_str()
+
 func add_memory_tile(image: Texture2D, frame: Texture2D) -> void:
 	var mt: MemoryTile = MEMORY_TILE.instantiate()
 	tile_grid.add_child(mt)
 	mt.setup(image, frame)
-	
+
 func on_level_selected(level_num: int) -> void:
 	var lds: LevelDataSelector = LevelDataSelector.get_level_selection(level_num)
 	var fi: Texture2D = ImageManager.get_random_frame_image()
@@ -28,7 +34,7 @@ func on_level_selected(level_num: int) -> void:
 		add_memory_tile(im, fi)
 		
 	scorer.clear_new_game(lds.get_target_pairs())
-	
+
 func _on_exit_button_pressed() -> void:
 	for t in tile_grid.get_children():
 		t.queue_free()
